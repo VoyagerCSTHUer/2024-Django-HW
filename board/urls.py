9cc9b4f5c1a2a118b53abd16e0eaeb1892439571
Voyager <cspxingren_1118@qq.com>
2024-09-20 16:15:37 +0800

    adding route

--- board/urls.py
+++ board/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('login', views.login),
     path('boards', views.boards),
     # TODO Start: [Student] add routing paths for `boards/<index>` and `user/<userName>`
-
+    path('boards/<index>', views.board),
+    path('user/<userName>', views.user),
     # TODO End: [Student] add routing paths for `boards/<index>` and `user/<userName>`
 
 ]
